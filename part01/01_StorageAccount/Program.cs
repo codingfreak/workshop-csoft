d57f67781ce68f45fdb57f697fb3ac6c6c15483f
alexander.schmidt <alexander.schmidt@devdeer.com>
2019-07-30 17:17:14 +0200

    First sample for storage upload finished.

---
+++ part01/01_StorageAccount/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _01_StorageAccount
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // prepare config system
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var configuration = builder.Build();
+            // init storage account
+            var storageAccountConnectionString = configuration["Azure:StorageConnectionString"];
+            var account = CloudStorageAccount.Parse(storageAccountConnectionString);
+            var blobClient = account.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(configuration["Azure:ContainerName"]);
+            // ensure that the container exists
+            await container.CreateIfNotExistsAsync();
+            //await container.SetPermissionsAsync(new BlobContainerPermissions
+            //{
+            //    PublicAccess = BlobContainerPublicAccessType.Blob
+            //});
+            // upload files from local folder
+            Console.WriteLine($"Using BLOB container '{container.Name}'");
+            var directory = new DirectoryInfo(configuration["SourcePath"]);
+            var files = directory.GetFiles();
+            Console.WriteLine($"Uploading {files.Length} files...");
+            foreach(var file in files)
+            {
+                var blob = container.GetBlockBlobReference(file.Name);
+                Console.Write($"Uploading {file.Name}...");
+                await blob.UploadFromFileAsync(file.FullName);
+                Console.WriteLine("Done");
+            }
+            Console.WriteLine("Upload completed.");
+            Console.ReadKey();
+        }
+    }
+}
